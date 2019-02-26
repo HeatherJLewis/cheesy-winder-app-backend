bb07ff6d0a864e3649fdca94fa81f3ab867f6b8c
emwalks <walkere@tcd.ie>
2019-02-26 21:55:37 +0000

    updated wine.js to have app.get function

--- wine.js
+++ wine.js
@@ -2,14 +2,31 @@ const serverless = require('serverless-http');
 const express = require('express');
 const app = express();
 
-app.get('/wine', function (request, response) {
+const cors = require('cors');
 
-  const username = request.query.username;
+app.use(express.json());
+app.use(cors());
+//this cors middleware adds a header to allow connections
+//this registers the middleware we need as long as you have express 4.16
 
-  const someJson = {
-    message: "Hello " + username
-  };
-  response.json(someJson);
+const databaseService = require('./databaseservice')
+
+//app.get is our request type for GET
+app.get('/wine/:cheeseName', function (request, response) {
+
+  databaseService.getWine()
+    .then(function (results) {
+      //We got the tasks OK
+      response.json(results);
+    })
+
+    .catch(function (error) {
+      //something went wrong when getting the tasks
+      response.status(500);
+      response.json(error);
+
+    });
+
 })
 
 module.exports.handler = serverless(app);
